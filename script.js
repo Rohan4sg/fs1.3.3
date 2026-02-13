269e7d1153b50c08080e7dbfa79202071e0fcbce
Rohan <rohansuraniya@gmail.com>
2026-02-13 23:06:34 +0530

    Clean HTML page

--- script.js
+++ script.js
@@ -5,41 +5,39 @@ class Person {
   }
 
   displayInfo() {
-    console.log(`Name: ${this.name}, Age: ${this.age}`);
+    return `Name: ${this.name}, Age: ${this.age}`;
   }
 }
 
 class Student extends Person {
   constructor(name, age, rollNo, course) {
-    super(name, age); 
+    super(name, age);
     this.rollNo = rollNo;
     this.course = course;
   }
 
   displayStudent() {
-    this.displayInfo();
-    console.log(`Roll No: ${this.rollNo}, Course: ${this.course}`);
+    return `${this.displayInfo()}<br>Roll No: ${this.rollNo}, Course: ${this.course}`;
   }
 }
 
 class Teacher extends Person {
   constructor(name, age, empId, subject) {
-    super(name, age); 
+    super(name, age);
     this.empId = empId;
     this.subject = subject;
   }
 
   displayTeacher() {
-    this.displayInfo();
-    console.log(`Employee ID: ${this.empId}, Subject: ${this.subject}`);
+    return `${this.displayInfo()}<br>Employee ID: ${this.empId}, Subject: ${this.subject}`;
   }
 }
 
 const student1 = new Student("Rohan", 20, "CS101", "Computer Science");
 const teacher1 = new Teacher("Dr. Sharma", 45, "T202", "Data Structures");
 
-console.log("Student Details:");
-student1.displayStudent();
+document.body.innerHTML += "<h3>Student Details</h3>";
+document.body.innerHTML += student1.displayStudent();
 
-console.log("\nTeacher Details:");
-teacher1.displayTeacher();
+document.body.innerHTML += "<h3>Teacher Details</h3>";
+document.body.innerHTML += teacher1.displayTeacher();
